4b05d302b021210d9ec5fb8ff3d62e965b6b68ca
SeokWoo Lee <skwu1@naver.com>
2017-08-16 17:28:45 +0900

    [iOS] Keyboard Show/Hide Notification 등록 및 Frame 조절 메소드 추가.

--- iOS/TempProject/TMPViewController.m
+++ iOS/TempProject/TMPViewController.m
@@ -14,18 +14,63 @@
 
 @implementation TMPViewController
 
+#pragma mark - Life Cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     [self setNeedsStatusBarAppearanceUpdate];
+    [self registerForKeyboardNotifications];
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:UIKeyboardDidShowNotification];
+    [[NSNotificationCenter defaultCenter] removeObserver:UIKeyboardWillHideNotification];
+}
+
+
+#pragma mark - Status Bar
+
 - (UIStatusBarStyle) preferredStatusBarStyle {
     return UIStatusBarStyleLightContent;
+}
+
+
+#pragma mark - Keyboard
+
+- (void)registerForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWasShown:) name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillBeHidden:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+
+- (void)keyboardWasShown:(NSNotification *)notification
+{
+    NSDictionary *userInfo = notification.userInfo;
+    CGSize keyboardFrame = [[userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    CGRect viewFrame = self.view.frame;
+    viewFrame.size.height -= keyboardFrame.height;
+    [self.view setFrame:viewFrame];
+}
+
+
+- (void)keyboardWillBeHidden:(NSNotification *)notification
+{
+    NSDictionary *userInfo = notification.userInfo;
+    CGSize keyboardFrame = [[userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    CGRect viewFrame = self.view.frame;
+    viewFrame.size.height += keyboardFrame.height;
+    [self.view setFrame:viewFrame];
 }
 
 @end
