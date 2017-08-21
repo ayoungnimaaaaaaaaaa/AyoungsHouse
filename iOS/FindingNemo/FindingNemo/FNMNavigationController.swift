a1fc3606cf9758a9fa4ead6060998841d559d710
SeokWoo Lee <skwu1@naver.com>
2017-08-21 22:45:23 +0900

    [iOS] FNMNavigationController 생성.

---
+++ iOS/FindingNemo/FindingNemo/FNMNavigationController.swift
@@ -0,0 +1,20 @@
+//
+//  FNMNavigationController.swift
+//  FindingNemo
+//
+//  Created by SeokWoo Lee on 2017. 8. 21..
+//  Copyright © 2017년 SeokWoo Lee. All rights reserved.
+//
+
+import UIKit
+
+class FNMNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
