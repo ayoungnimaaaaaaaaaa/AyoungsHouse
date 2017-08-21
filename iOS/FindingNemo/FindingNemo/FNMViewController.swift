5dcb1933a24c27855dfc5da9fc31941a92010fa7
SeokWoo Lee <skwu1@naver.com>
2017-08-21 22:44:36 +0900

    [iOS] 공통으로 적용되는 것이 들어갈 FNMViewController 생성.

---
+++ iOS/FindingNemo/FindingNemo/FNMViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FNMViewController.swift
+//  FindingNemo
+//
+//  Created by SeokWoo Lee on 2017. 8. 21..
+//  Copyright © 2017년 SeokWoo Lee. All rights reserved.
+//
+
+import UIKit
+
+class FNMViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
