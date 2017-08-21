18cf7a0d61bc6f2341088423c63f05e4de5636f6
SeokWoo Lee <skwu1@naver.com>
2017-08-21 23:30:51 +0900

    [iOS] 공통 Search Bar 생성 및 스타일 적용.

---
+++ iOS/FindingNemo/FindingNemo/FNMSearchBar.swift
@@ -0,0 +1,15 @@
+//
+//  FNMSearchBar.swift
+//  FindingNemo
+//
+//  Created by SeokWoo Lee on 2017. 8. 21..
+//  Copyright © 2017년 SeokWoo Lee. All rights reserved.
+//
+
+import UIKit
+
+class FNMSearchBar: UISearchBar {
+    override func awakeFromNib() {
+        self.searchBarStyle = UISearchBarStyle.minimal
+    }
+}
