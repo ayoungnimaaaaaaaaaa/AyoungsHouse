e51877657cfadf706a27830084e540d9fd6216b5
SeokWoo Lee <skwu1@naver.com>
2017-09-03 18:05:44 +0900

    [iOS] - FNMRestaurantDetailViewController 대략적으로 만듦.

---
+++ iOS/FindingNemo/FindingNemo/Common/Controller/FNMRestaurantDetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  FNMRestaurantDetailViewController.swift
+//  FindingNemo
+//
+//  Created by SeokWoo Lee on 2017. 9. 3..
+//  Copyright © 2017년 SeokWoo Lee. All rights reserved.
+//
+
+import UIKit
+
+class FNMRestaurantDetailViewController: FNMViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    private func setupNavigationBar() {
+        self.navigationItem.title = "식당 이름"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "리뷰 작성", style: .plain, target: self, action: nil)
+    }
+}
+
+//  MARK: UITableViewDataSource
+extension FNMRestaurantDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+//  MARK: UITableViewDelegate
+extension FNMRestaurantDetailViewController: UITableViewDelegate {
+    
+}
+
+enum FNMRestaurantDetailItem {
+    case FNMRestaurantDetailItemThumbnail
+}
