8f949bca13731535edbab363cea3763fa13a4e54
SeokWoo Lee <skwu1@naver.com>
2017-09-03 23:54:32 +0900

    [iOS] FNMMoreViewController 생성.

---
+++ iOS/FindingNemo/FindingNemo/More/FNMMoreViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FNMMoreViewController.swift
+//  FindingNemo
+//
+//  Created by SeokWoo Lee on 2017. 9. 3..
+//  Copyright © 2017년 SeokWoo Lee. All rights reserved.
+//
+
+import UIKit
+
+class FNMMoreViewController: FNMViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
