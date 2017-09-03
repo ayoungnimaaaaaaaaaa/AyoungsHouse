fb9dc2345f867401b51b73268931593a29073305
SeokWoo Lee <skwu1@naver.com>
2017-09-03 23:31:00 +0900

    [iOS] FNMFeedViewController 추가.

---
+++ iOS/FindingNemo/FindingNemo/Feed/FNMFeedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FNMFeedViewController.swift
+//  FindingNemo
+//
+//  Created by SeokWoo Lee on 2017. 9. 3..
+//  Copyright © 2017년 SeokWoo Lee. All rights reserved.
+//
+
+import UIKit
+
+class FNMFeedViewController: FNMViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+extension FNMFeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension FNMFeedViewController: UITableViewDelegate {
+    
+}
